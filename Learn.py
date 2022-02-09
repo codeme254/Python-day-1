fc1332e2bebe2b5cfa3918319b1f305af9dd2dc2
otwoma <otwomadennis365@gmail.com>
2022-02-09 18:58:18 +0300

    initial commit

---
+++ Learn.py
@@ -0,0 +1,43 @@
+#this is a python comment, started with the pound symbol
+
+#printing to the console in python
+print("Hello world!")
+
+#coding challenge[interactive coding exercise]
+print("Day 1 - Python print function")
+print("The function is declared like this: ")
+print("print(\"what to print\")")
+
+#strings manipulation
+#single print method to print multiple lines
+print("Hello world!\nHellow world!\nHello world!")
+
+#string concatenation
+print("Hello" + " Dennis")
+print("Hello" +" "+"Dennis again")
+#spaces in python are very important, start off all your code at the beginning of the line
+
+#the python input function
+#input("What is your name? ")
+print("Hello "+input("What is your name? "))
+
+#input function exercise
+#print the number of characters in a user name
+print("Your name has "+str(len(input("What is you name> ")))+" characters");
+
+#python variables
+name = input("What is you good name? ")
+print(name)
+
+#variables coding exercise
+#switching the values of two variables
+a = input("Enter a: ")
+b = input("Enter b: ")
+print("A before is = "+a)
+print("B before is = "+b)
+c = a
+a = b
+b = c
+print("A after is "+a)
+print("B after is "+b)
+
---
+++ project.py
@@ -0,0 +1,11 @@
+#BAND NAME GENERATOR
+
+#create a greeting for your program
+#ask the user for the city they grew up in
+#ask for the user for the name of the pet
+#combine the name of their city and pet and show their band name
+#make sure the input cursor show on a new line
+print("WELCOME TO THE BAND NAME GENERATOR\n")
+city = input("Which city did you grow in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name could be "+ city+" "+pet_name)
